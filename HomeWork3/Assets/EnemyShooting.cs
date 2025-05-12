efba770112a1c58741cd911decf49fd56131d131
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-05-11 18:49:38 -0700

    enemy shooting start

---
+++ HomeWork3/Assets/EnemyShooting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform[] bulletSpawnPoints; 
+    [SerializeField] private float Min_Angle = 0f;
+    [SerializeField] private float Max_Angle = 360f;
+    private float timePassed =0f;
+   void Start()
+   {
+    foreach(Transform spawnPoint in bulletSpawnPoints){
+    float randomAngle = Random.Range(Min_Angle, Max_Angle);
+    SpawnPoint.rotation = Quaternion.Euler(0, 0, randomAngle);
+    }
+   }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+            if (timePassed > 30f ){
+              
+
+               foreach (Transform SpawnPoint in bulletSpawnPoints)
+            {
+                Instantiate(bulletPrefab, SpawnPoint.position, SpawnPoint.rotation);
+                
+            }
+            timePassed = 0f;
+            }
+
+
+        
+    }
+}
